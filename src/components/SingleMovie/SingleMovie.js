f2ddef69c11300969f7360ab6dfeac5a80012d5f
RingiBoy <mitya.info@gmail.com>
2022-06-20 19:51:48 +0300

    single movie bug

--- src/components/SingleMovie/SingleMovie.js
+++ src/components/SingleMovie/SingleMovie.js
@@ -1,23 +1,27 @@
 import React, {useEffect} from 'react';
 
 import {useParams} from "react-router-dom";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {movieActions} from "../../redux/slices/movie.slice";
+import css from "./SingleMovie.module.css"
 
 
 const SingleMovie = () => {
 
-
+    const {movie} = useSelector(state => state.movies)
+    // useSelector(state => console.log(state))
     const dispatch = useDispatch()
     const {id} = useParams()
 
     useEffect(() => {
         dispatch(movieActions.getSingleMovie(id))
-    }, [])
+    }, [id])
 
     return (
-        <div>
+        <div className={css.main}>
             {id}
+            {movie.homepage}
+            {movie.release_date}
         </div>
     );
 };
