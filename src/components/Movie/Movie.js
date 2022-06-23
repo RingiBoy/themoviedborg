1ae0c7d880883f8e03686e3d9f59f5cb2172f69e
RingiBoy <mitya.info@gmail.com>
2022-06-23 23:03:14 +0300

    shtrih

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import css from "./Movie.module.css"
-import {Link} from "react-router-dom";
+import css from "./Movie.module.css";
+import { Link } from "react-router-dom";
 
-const Movie = ({movie:{original_title, release_date, vote_average, backdrop_path, id}}) => {
-    return (
-        <div className={css.movie}>
-
-
-            <img className={css.image} src={`https://www.themoviedb.org/t/p/w220_and_h330_face${backdrop_path}`}/>
-            <p className={css.nameFilm}>{original_title}</p>
-            <p>{release_date}</p>
-            <p>{vote_average}</p>
-            <Link to={id.toString()}>get Details</Link>
-
-        </div>
-    );
+const Movie = ({
+  movie: { original_title, release_date, vote_average, backdrop_path, id },
+}) => {
+  return (
+    <div className={css.movie}>
+      <Link to={id.toString()}>
+        <img
+          className={css.image}
+          src={`https://www.themoviedb.org/t/p/w220_and_h330_face${backdrop_path}`}
+        />
+      </Link>
+      <p className={css.nameFilm}>{original_title}</p>
+      <p>{release_date}</p>
+      <p>{vote_average}</p>
+    </div>
+  );
 };
 
 export default Movie;
-
 
 // https://www.themoviedb.org/t/p/w440_and_h660_face/w2loOtr8rYmIh13xyKCYdWHu58F.jpg
