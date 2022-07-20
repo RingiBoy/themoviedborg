58cf17c6ff58569154daeec93b4664e3a645bbb0
RingiBoy <mitya.info@gmail.com>
2022-07-20 21:47:31 +0300

    picture

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -7,25 +7,45 @@ import Stars from "../Stars/Stars";
 const Movie = ({
   movie: {title, release_date, vote_average, backdrop_path, id },
 }) => {
- // if(backdrop_path){
+ if(backdrop_path){
      return (
 
          <div className={css.movie}>
              <Link to={id.toString()}>
-                 <img alt={''}
+                 <img alt={'title'}   
                      className={css.image}
+                     
                      src={`https://www.themoviedb.org/t/p/w220_and_h330_face${backdrop_path}`}
                  />
              </Link>
              <p className={css.nameFilm}>{title}</p>
              <p>{release_date}</p>
-             {/*<p>{vote_average}</p>*/}
+            
              <Stars vote_average={vote_average}/>
          </div>
 
 
      );
- // }
+ }
+ return (
+
+    <div className={css.movie}>
+        <Link to={id.toString()}>
+            <img alt={'title'}   
+                className={css.image}
+                
+                src={`https://us.123rf.com/450wm/mathier/mathier1905/mathier190500002/134557216-no-thumbnail-image-placeholder-for-forums-blogs-and-websites.jpg`}
+            />
+        </Link>
+        <p className={css.nameFilm}>{title}</p>
+        <p>{release_date}</p>
+       
+        <Stars vote_average={vote_average}/>
+    </div>
+
+
+);
+ 
 
 };
 
